106fd14ad3247b104d893989fbd71688835b2bb1
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2022-10-15 21:31:28 +0300

    update files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+const divRef = document.querySelector('.gallery');
+const arrayGallery = galleryItems.map(image => {
+  return `<a class="gallery__link" href="${image.original}">
+      <img
+        class="gallery__image"
+        src="${image.preview}"
+            alt="${image.description}"
+      />
+    </a>`;
+});
+divRef.insertAdjacentHTML('afterbegin', arrayGallery.join(''));
+console.log(arrayGallery);
+divRef.addEventListener('click', event => {
+  event.preventDefault();
+});
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
 
+import player from '@vimeo/player';
+
+    const iframe = document.querySelector('#vimeo-player');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function () {
+      console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function (title) {
+      console.log('title:', title);
+    });
+
+const onPlay = function (timeupdate) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
