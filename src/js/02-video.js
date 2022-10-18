90325ff27f0a1c8530083cb36db01967b778dbb8
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2022-10-18 21:02:00 +0300

    update js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,37 +1,13 @@
+import throttle from 'lodash.throttle';
+import vimeoPlayer from '@vimeo/player';
 
-import player from '@vimeo/player';
+const iframeRef = document.querySelector('#vimeo-player');
+console.log(iframeRef);
+const player = new vimeoPlayer(iframeRef);
 
-    const iframe = document.querySelector('#vimeo-player');
-    const player = new Vimeo.Player(iframe);
-
-    player.on('play', function () {
-      console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function (title) {
-      console.log('title:', title);
-    });
-
-const onPlay = function (timeupdate) {
-  // data is an object containing properties specific to that event
-};
-
-player.on('play', onPlay);
-
-
-player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+const onPlay = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player.on('timeupdate', onPlay);
+player.setCurrentTime(savedTime);
